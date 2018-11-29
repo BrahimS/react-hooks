84a2c3483b4c600610ead8c333afe6e74f380957
BS <bs@MacBook-Pro-de-BS.local>
2018-11-29 16:34:28 +0100

    Add the Todo function

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React, { useState } from 'react'
+function Todo ({ todo, index}) {
+  return (
+    <div className="Todo">{todo.text}</div>
+  )
 }
 
-export default App;
+function App ()  {
+  const [ todos, setTodos ] = useState([
+    { 
+      text: "First task",
+      isCompleted: false
+    },
+    { 
+      text: "Second task",
+      isCompleted: false
+    },
+    { 
+      text: "Third task",
+      isCompleted: false
+    },
+  ])
+  return (
+    <div className="App">
+      <div className="todo-list">
+      {todos.map((todo, index) => (
+        <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
